dea3017b804ce4dd23c98ea06d91d937edcb136b
Shraddha Sahu <156925474+shraddha1558@users.noreply.github.com>
2024-09-23 06:36:20 +0000

    Missing and Repeating element in array

--- DE Shaw/Missing and Repeating.cpp
+++ DE Shaw/Missing and Repeating.cpp
@@ -35,7 +35,7 @@ class Solution {
             return repeating;
         }
 };
-
+// TC : O(N) SC: O(N)
 int main() {
     vector<int> arr = {1, 3, 4, 5, 5}; 
     int n = arr.size();
@@ -46,3 +46,25 @@ int main() {
     
     return 0;
 }
+
+//Leetcode solution
+//using hashing
+// class Solution {
+//   public:
+//     vector<int> findTwoElement(vector<int>& arr) {
+//         int n=arr.size();
+//         int hash[n+1]={0};
+//         for(int i=0;i<n;i++){
+//             hash[arr[i]]++;
+//         }
+//         int repeating =-1, missing =-1;
+//         for(int i=1;i<=n;i++){
+//             if(hash[i]>=2) repeating=i;
+//             else if(hash[i]==0) missing=i;
+//             if(repeating != -1 && missing != -1)
+//             break;
+//         }
+//         return {repeating, missing};
+//     }
+// };
+// TC: O(N) SC:O(1)
